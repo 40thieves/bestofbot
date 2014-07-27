121af975c3a1185cc5dee34e0080fee0f60d126a
Alasdair Smith <alasdairsmith100@gmail.com>
2014-07-27 16:11:15 +0100

    Adds more test cases for non-matching messages

--- test/filterSpec.js
+++ test/filterSpec.js
@@ -4,20 +4,23 @@ var expect = require('chai').expect
 ;
 
 describe('Filter', function() {
-	describe('test', function() {
-		var filter = new Filter({
-			filter: {
-				command: '!b ',
-				data: '(\\w+)+'
-			}
-		});
+	var filter = new Filter({
+		filter: {
+			command: '!b( )+',
+			data: '(\\w+)+'
+		}
+	});
 
+	describe('test', function() {
 		it('returns truthy for messages that match the filter', function() {
 			expect(filter.test('!b now')).to.be.ok;
+			expect(filter.test('!b  now')).to.be.ok;
 		});
 
 		it('returns false for messages that do not match the filter', function() {
 			expect(filter.test('foo')).to.be.false;
+			expect(filter.test('!s foo')).to.be.false;
+			expect(filter.test('!bfoo')).to.be.false;
 		});
 	});
 });
